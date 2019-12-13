1ce1dd28ed42bcef6bf85ea171852bc3faa91b96
Zbigniew Pękala <zbyszek26104@gmail.com>
2019-12-13 13:46:20 +0100

    Zsh changes

--- zsh/.zsh/env.zsh
+++ zsh/.zsh/env.zsh
@@ -1,11 +1,11 @@
 export ZSH=/Users/zbigniewpekala/.oh-my-zsh
 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
+export PATH=/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH
 export PATH=$PATH:~/src/priv/erlang/bumperl/_build/default/bin
 export PATH=$PATH:/opt/apache-cassandra-3.9/bin
+export PATH=$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH 
 
 export SED=gsed
-export RABBITMQCTL="/Users/zbigniewpekala/src/siftlogic/rabbitmq_server-3.6.1/sbin/rabbitmqctl"
 export ERL_AFLAGS="-kernel shell_history enabled"
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -4,3 +4,4 @@ source ~/.zsh/functions.zsh
 source ~/.zsh/plugins.zsh
 source ~/.zsh/aliases.zsh
 source ~/.priv.zsh
+source /usr/local/etc/profile.d/z.sh
